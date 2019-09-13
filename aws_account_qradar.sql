924a5c61c9d9898f4234c3a674020e10fe666ace
John Doe <39686917+cybertrop@users.noreply.github.com>
2019-09-13 16:01:47 -0500

    Update aws_account_qradar.sql

--- aws_account_qradar.sql
+++ aws_account_qradar.sql
@@ -1,5 +1,8 @@
-# This is a simple QRADAR query/ script to parse for AWS Account activity via associated API calls (CloudTrail)
-# Please note the fields in your QRadar instance might be different from these, so a little extraction might be necessary
+/*======================================================================================
+ This is a simple QRADAR query/ script to parse for AWS Account activity via associated API calls (CloudTrail)
+ *** Please note the fields in your QRadar instance might be different from these, so a little extraction might be necessary
+ ======================================================================================*/
+
 
 SELECT DATEFORMAT(starttime,
          'EEE, MM-d-yyyy, HH:mm:ss.SS') AS 'Time', "AWS: AccountID" AS "AWS Account", username, "sourceIP" AS 'Source IP', QIDNAME(qid) AS 'Event Name', LOGSOURCENAME(logsourceid) AS 'Source', UTF8(payload) AS 'Payload'
